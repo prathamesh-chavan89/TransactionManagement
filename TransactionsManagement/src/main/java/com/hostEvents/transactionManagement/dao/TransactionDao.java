934a96c07bf0f2f1eb934978c851428abeb9b691
Prathamesh Chavan <68934078+prathamesh-chavan89@users.noreply.github.com>
2021-09-19 21:46:36 +0530

    Delete TransactionsManagement directory

--- TransactionsManagement/src/main/java/com/hostEvents/transactionManagement/dao/TransactionDao.java
+++
@@ -1,5 +0,0 @@
-package com.hostEvents.transactionManagement.dao;
-
-public interface TransactionDao {
-
-}
--- TransactionsManagement/src/main/java/com/hostEvents/transactionManagement/dao/impl/TransactionDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package com.hostEvents.transactionManagement.dao.impl;
-
-import com.hostEvents.transactionManagement.dao.TransactionDao;
-
-public class TransactionDaoImpl implements TransactionDao {
-
-}
